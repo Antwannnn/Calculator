d27e8d8cbc0444692fa7f18c87f2da02a933c31f
Atsura <94127380+Antwannnn@users.noreply.github.com>
2023-02-16 18:41:36 +0100

    base is working

--- script.js
+++ script.js
@@ -1,28 +1,27 @@
 
 const numbers = document.querySelectorAll('.numbers');
 const operators = document.querySelectorAll('.operators');
+const equal = document.getElementById('equals');
+const clear = document.getElementById('clear');
 
 let result = document.getElementById('result');
+let calc = null;
 
 let baseColor = getComputedStyle(document.querySelector('.numbers'));
 
 let operating = false;
-let left;
-let right;
-    
+let operands = [];
+let operator;
+
 // Navigate through the numbers' td each click to get the clicked element.
 numbers.forEach(current =>{
     current.addEventListener('click', function (){
-        if(operating){
-            clearContent(result);
-            left = current.textContent;
-        }
-        else{
-            right = current.textContent; 
-        }
+        var value = current.textContent;
 
-        addToResult(current, result);
+        addToResult(value, result);
         changeColor(current, 'yellow');
+        operands.push(value);
+        console.log(operands);
         delayTask(1000, function (){
             changeColor(current, baseColor)
         });
@@ -31,34 +30,51 @@ numbers.forEach(current =>{
 
 operators.forEach(current => {
     current.addEventListener('click', function(){
-        if(result.textContent !== ''){
-            clearContent(result)
-            addToResult(current, result)
+        var resultContent = result.textContent;
+        if(resultContent !== ''){
+            addToResult(current.textContent, result);
+            operator = current.textContent;
+            console.log(operands);
             operating = true;
+            current.setAttribute('style', 'font-size : 55px; opacity : 0.8;');
+
         }
 
     });
 })
 
-function getOperatorString(str){
+clear.addEventListener('click', function() {
+    clearContent(result);
+    operands = [];
+    operator = undefined;
+    operating = false;
+    calc = null;
+})
+
+equal.addEventListener('click', function() {
+    clearContent(result);
+    calc = calculate(operator);
+    console.log(calc);
+    addToResult(calc, result)
+})
+
+function toOperation(str, n1, n2){
     switch(str){
-        case '*': return Operators.multiply;
-        case '÷': return Operators.divide;
-        case '-': return Operators.minus;
-        case '+': return Operators.plus;
-        case '%': return Operators.modulo;
-        case 'cl'.toLowerCase : return Operators.clear;
+        case 'X': return n1 * n2;
+        case '÷': return n1 / n2;
+        case '-': return n1 - n2;
+        case '+': return n1 + n2;
+        case '%': return n1 % n2;
+        default : return '';
     }
 }
 
-var toOperator = {
-    '+' : function (x, y) { x + y },
-    '-' : function (x, y) { x - y},
-    '/' : function (x, y) { x / y},
-    '*' : function (x, y) { x * y},
-    '%' : function (x, y) { x % y},
-    'cl' : function (element) { clearContent(element) }
-};
+
+function calculate(operator){
+    let left = tryParseNumber(operands[0], 10);
+    let right = tryParseNumber(operands[1], 10);
+    return toOperation(operator, left, right);
+}
 
 // Delay task using setTimeout() function
 const delayTask = async (delay, task) => {
@@ -74,7 +90,7 @@ function changeColor(element, color){
 
 // Adds an element to the result.
 function addToResult(element, result){
-    result.textContent += element.textContent;
+    result.textContent += element;
 }
 
 // Clear the content of the result
@@ -83,12 +99,11 @@ function clearContent(element){
 }
 
 // Return an integer based on string content in a certain radix
-function parseNumber(str, base){
+function tryParseNumber(str, base){
     try{
         return parseInt(str, base);
     } catch(e){
         throw new NumberFormatException("Couldn't parse number to numeric value.")
     }
 }
-
 
